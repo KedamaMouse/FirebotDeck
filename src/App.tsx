04823cb0ee032018c31730423e00a49fe617874d
KedamaMouse <2153956+KedamaMouse@users.noreply.github.com>
2025-11-18 22:48:48 -0600

    some style updates, a new build

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import './App.css';
+import styled,{ css } from "styled-components";
+
 import { DeckButton } from './DeckButton';
 
 function App() {
 
 
   return (
-    <div className="App">
-
-    <div className="row"> 
-      <div className="col">
-
-      <DeckButton caption='a caption! ' actionID='a62e08df-c863-404c-b0c0-379f3bb75e0c' ipAddress='localhost' port='7472' />
-    </div>
-     </div>
-      </div>
+    <Container >
+      <DeckButton backgroundColor='blue' textColor='white' caption='a caption! ' actionID='a62e08df-c863-404c-b0c0-379f3bb75e0c' ipAddress='localhost' port='7472' />
+    </Container>
    
 
          
 
   );
 }
+
+const Container =styled.div`
+background-color: black;
+height: 100%;
+`
 
 export default App;
--- src/DeckButton.tsx
+++ src/DeckButton.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-
+import styled,{ css } from "styled-components";
 
 interface IDeckButtonProps
 {
@@ -7,12 +7,19 @@ interface IDeckButtonProps
     actionID: string;
     ipAddress: string;
     port: string;
+    backgroundColor: string;
+    textColor: string;
 }
 
 export const DeckButton:React.FC<IDeckButtonProps> = (props) => {
     const onclick= () => {
     fetch('http://'+props.ipAddress+':'+props.port+'/api/v1/effects/preset/' + props.actionID);
     }
-    return <button className="btn btn-primary" onClick={onclick} >{props.caption}</button>
+    return <Button style={{color: props.textColor, backgroundColor: props.backgroundColor}} onClick={onclick} >{props.caption}</Button>
 
 }
+
+const Button = styled.button`
+    font-size: 50px;
+    border-radius: 5px;
+`
