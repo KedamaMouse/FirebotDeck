5e6b8995f5aca5caadcb635947b8bd66489fa352
KedamaMouse <2153956+KedamaMouse@users.noreply.github.com>
2025-11-23 21:13:51 -0600

    update instructions

--- src/Instructions.tsx
+++ src/Instructions.tsx
@@ -6,9 +6,9 @@ export const Instructions:React.FC = () => {
 
     return <Div>
         1. Make actions you want to trigger in firebot as "Preset effect lists" <br/>
-        2. In Firebot go to Settings  Setups Create Setup<br/>
-        3. Make a setup with your preset effects<br/>
-        4. choose file below to import the setup<br/>
+        2. If you're connecting from a different machine, enter the IP firebot is running on<br/>
+        3. Refresh this page<br/>
+        4. choose your effects from the dropdown to add them as buttons<br/>
     </Div>
 }
 
