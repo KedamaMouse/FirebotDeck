21fdb434ff6d759f773c56a5640b44cb3a4cf3f1
KedamaMouse <2153956+KedamaMouse@users.noreply.github.com>
2025-11-26 18:17:14 -0600

    add option to change caption

--- src/DeckButton.tsx
+++ src/DeckButton.tsx
@@ -1,78 +1,94 @@
 
 import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuPortal, ContextMenuTrigger } from "@radix-ui/react-context-menu";
-import styled,{ css } from "styled-components";
+import styled, { css } from "styled-components";
 import { IDeckButton } from "./App";
 
 
-interface IDeckButtonProps
-{
-    caption : string;
+interface IDeckButtonProps {
+    caption: string;
     actionID: string;
     baseApiURL: string;
     backgroundColor: string;
     textColor: string;
-    removeButtonHandler:() => void;
-    afterActionHandler: () => void; 
+    removeButtonHandler: () => void;
+    afterActionHandler: () => void;
     updateButtonProps: (props: Partial<IDeckButton>) => void;
     backColorVariable: string;
 }
 
-export const DeckButton:React.FC<IDeckButtonProps> = (props) => {
-    const onclick= async () => {
-        const url =props.baseApiURL + '/effects/preset/' + props.actionID;
+export const DeckButton: React.FC<IDeckButtonProps> = (props) => {
+    const onclick = async () => {
+        const url = props.baseApiURL + '/effects/preset/' + props.actionID;
         try {
-              await fetch(url);
-              props.afterActionHandler();
+            await fetch(url);
+            props.afterActionHandler();
         } catch (error) {
-            alert("request failed. is firebot running? Request: "+ url);
+            alert("request failed. is firebot running? Request: " + url);
         }
-           
-       
+
+
     }
     return <>
-    <ContextMenu>
-    <ContextMenuTrigger>
+        <ContextMenu>
+            <ContextMenuTrigger>
+                <Button style={{ color: props.textColor, backgroundColor: props.backgroundColor }} onClick={onclick} >{props.caption}</Button>
+            </ContextMenuTrigger>
+            <ContextMenuPortal>
 
-        <Button style={{color: props.textColor, backgroundColor: props.backgroundColor}} onClick={onclick} >{props.caption}</Button>
-    </ContextMenuTrigger>
-        <ContextMenuPortal>
+            </ContextMenuPortal>
+            <ContextMenuContent>
+                <ContentMenuDiv>
+                    <ContextMenuItem onClick={() => props.removeButtonHandler()}>
+                        <ContextMenuItemDiv>Remove button</ContextMenuItemDiv>
+                    </ContextMenuItem>
 
-        </ContextMenuPortal>
-        <ContextMenuContent>
-        <ContextMenuItem onClick={() => props.removeButtonHandler()}>
-            <ContextMenuItemDiv>Remove Button</ContextMenuItemDiv>
-        </ContextMenuItem>
+                    <ContextMenuItem onClick={() => {
+                        const variable = prompt("Firebot custom variable name to look to. current value: '" + props.backColorVariable + "'");
+                        if (variable) {
+                            props.updateButtonProps({ backColorVariable: variable });
+                        }
+                    }
+                    }>
+                        <ContextMenuItemDiv>Set color variable</ContextMenuItemDiv>
+                    </ContextMenuItem>
 
-        <ContextMenuItem onClick={() => {
-            const variable=prompt("Firebot custom variable name to look to. current value: '"+props.backColorVariable+"'");
-            if(variable){
-                props.updateButtonProps({backColorVariable: variable});
-            }
-        }
-        }>
-            <ContextMenuItemDiv>set color variable</ContextMenuItemDiv>
-        </ContextMenuItem>
-        
-        <ContextMenuItem onClick={() => {
-            const color=prompt("Enter an html color code");
-            if(color){
-                props.updateButtonProps({backgroundColor: color});
-            }
-        }
-        }>
-            <ContextMenuItemDiv>set color</ContextMenuItemDiv>
-        </ContextMenuItem>
+                    <ContextMenuItem onClick={() => {
+                        const color = prompt("Enter an html color code");
+                        if (color) {
+                            props.updateButtonProps({ backgroundColor: color });
+                        }
+                    }
+                    }>
+                        <ContextMenuItemDiv>set color</ContextMenuItemDiv>
+                    </ContextMenuItem>
+                    
+                    <ContextMenuItem onClick={() => {
+                        const caption = prompt("Enter a button caption");
+                        if (caption) {
+                            props.updateButtonProps({ caption: caption });
+                        }
+                    }
+                    }>
+                        <ContextMenuItemDiv>Change button caption</ContextMenuItemDiv>
+                    </ContextMenuItem>
+                </ContentMenuDiv>
+            </ContextMenuContent>
 
-        </ContextMenuContent>
-
-    </ContextMenu>
+        </ContextMenu>
     </>
 
 }
 
-const ContextMenuItemDiv= styled.div`
+const ContentMenuDiv = styled.div`
+    padding-top: 2px;
+    padding-bottom: 2px;
     background-color: white;
     color: black;
+`
+
+const ContextMenuItemDiv = styled.div`
+    padding-left: 2px;
+    padding-right: 2px;
     &:hover
     {
         background-color: #e094d2;
